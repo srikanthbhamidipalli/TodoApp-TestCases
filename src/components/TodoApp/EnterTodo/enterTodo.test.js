1b439a91b7603e5d9be0032945bc8869b0391215
iB Admin <mohanagajula579@gmail.com>
2019-08-03 15:23:54 +0530

    [refactored state] should check for not to submit the todo when input is empty

--- src/components/TodoApp/EnterTodo/enterTodo.test.js
+++ src/components/TodoApp/EnterTodo/enterTodo.test.js
@@ -4,7 +4,15 @@ import EnterTodo from "./index";
 import TodoStore from "../../../Stores/TodoStore";
 
 describe("EnterTodo component testcases", () => {
+  let keyDownEvent;
   afterEach(cleanup);
+  beforeEach(() => {
+    keyDownEvent = {
+      key: "Enter",
+      keyCode: 13,
+      code: 13
+    };
+  });
   const todoStore = new TodoStore();
   it("should Take a todo description from InputTextBox", () => {
     // todoStore.addTodo = jest.fn();
@@ -21,11 +29,6 @@ describe("EnterTodo component testcases", () => {
     const todoDesc = "Learn Todos App";
     const changeEvent = { target: { value: todoDesc } };
     fireEvent.change(inputTextBox, changeEvent);
-    const keyDownEvent = {
-      key: "Enter",
-      keyCode: 13,
-      code: 13
-    };
     fireEvent.keyDown(inputTextBox, keyDownEvent);
     expect(todoStore.addTodo).toBeCalledWith(todoDesc);
   });
@@ -40,12 +43,6 @@ describe("EnterTodo component testcases", () => {
     );
     const inputTextBox = getByPlaceholderText("What needs to be done");
     const todoDesc = "";
-    const changeEvent = { target: { value: todoDesc } };
-    const keyDownEvent = {
-      key: "Enter",
-      keyCode: 13,
-      code: 13
-    };
     fireEvent.keyDown(inputTextBox, keyDownEvent);
     expect(todoStore.addTodo).not.toBeCalledWith(todoDesc);
   });
