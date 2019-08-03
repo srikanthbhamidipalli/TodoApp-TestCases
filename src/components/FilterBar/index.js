793d623ef1d508d856ed94335828adb3036ed6ae
iB Admin <mohanagajula579@gmail.com>
2019-08-03 12:16:10 +0530

    [green state] should check for the all button on click event

--- src/components/FilterBar/index.js
+++ src/components/FilterBar/index.js
@@ -4,13 +4,24 @@ class FilterBar extends Component {
   handleClearCompletedClick = () => {
     this.props.todoStore.clearCompletedTodos();
   };
+  handleAllButtonClick = e => {
+    this.props.todoStore.setFilterText(e.target.value);
+  };
   render() {
     return (
       <div>
         <span data-testid="items-left">
           {this.props.todoStore.activeTodosCount} items left
         </span>
-
+        <span>
+          <button
+            data-testid="all-button"
+            onClick={this.handleAllButtonClick}
+            value="all"
+          >
+            all
+          </button>
+        </span>
         <span
           onClick={this.handleClearCompletedClick}
           data-testid="clear-completed"
