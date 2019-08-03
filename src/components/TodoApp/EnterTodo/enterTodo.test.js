98024e330b030b504b8b1e49206f1c59e409db0a
iB Admin <mohanagajula579@gmail.com>
2019-08-03 15:14:12 +0530

    [red state] should check for not to submit the todo when input is empty

--- src/components/TodoApp/EnterTodo/enterTodo.test.js
+++ src/components/TodoApp/EnterTodo/enterTodo.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import EnterTodo from "./index";
 import TodoStore from "../../../Stores/TodoStore";
 
 describe("EnterTodo component testcases", () => {
+  afterEach(cleanup);
   const todoStore = new TodoStore();
   it("should Take a todo description from InputTextBox", () => {
     // todoStore.addTodo = jest.fn();
@@ -27,5 +28,25 @@ describe("EnterTodo component testcases", () => {
     };
     fireEvent.keyDown(inputTextBox, keyDownEvent);
     expect(todoStore.addTodo).toBeCalledWith(todoDesc);
+  });
+  it("should check for not to submit the todo when input text is empty", () => {
+    jest.spyOn(todoStore, "addTodo");
+    const { getByPlaceholderText } = render(
+      <EnterTodo
+        todoStore={todoStore}
+        inputText={""}
+        onPressEnterKey={todoStore.addTodo}
+      />
+    );
+    const inputTextBox = getByPlaceholderText("What needs to be done");
+    const todoDesc = "";
+    const changeEvent = { target: { value: todoDesc } };
+    const keyDownEvent = {
+      key: "Enter",
+      keyCode: 13,
+      code: 13
+    };
+    fireEvent.keyDown(inputTextBox, keyDownEvent);
+    expect(todoStore.addTodo).not.toBeCalledWith(todoDesc);
   });
 });
